54706ab2881213675ce7893405f014eb636428a1
samGH266 <ma.smith@ucdenver.edu>
2015-11-19 12:54:34 -0700

    Commitcachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,26 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(as.matrix(x)) {
+        inverse <- NULL           # initialize the inverse matrix
+        set <- function(y) {      # changes the input matrix stored in makeCacheMatrix. Use only if changes are needed
+                x <<- y                   # substitute input matrix x with y in makeCacheMatrix because using <<-
+                inverse <<- NULL       # restores null since old inverse not needed 		
+        }
+        get <- function() x       # returns the matrix x from makeCacheMatrix
+        setinv <- function(temp) inverse <<- temp   # store value of input in a variable temp in MakeCacheMatrixr
+        getinv <- function() inverse                              # return value of inverse in MakeCacheVector
+        list(set = set, get = get,          # stores four functions in makeCacheMatrix
+             setinv = setinv,
+             getinv = getinv)
 
 }
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) {    # cacheSolve input is object where makeCacheMatrix is stored
+        inv <- x$getinv()               
+        if(!is.null(m)) {                        # if inverse exists in memory, return a message and the value of inverse
+                message("getting cached inverse")
+                return(inverse                 
+        }           
+        data <- x$get()                  # retrieves vector stored in makeCacheMatrix  
+        inverse <- Solve(data)     # calculate inverse matrix
+        x$setinv(inverse)             # stores the inverse in the object assigned with makeCacheMatrix
+        inv
 }
+
